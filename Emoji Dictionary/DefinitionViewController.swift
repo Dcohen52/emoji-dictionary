8edde5d1ae411157fda058680f50b196550a2391
Dekel Cohen <dcohen52@gmail.com>
2017-03-12 21:19:00 +0200

    Pass Da Emoji

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Dekel Cohen on 12/03/2017.
+//  Copyright © 2017 Dekel Cohen. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    var emoji = "NO EMOJI"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print(emoji)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+}
